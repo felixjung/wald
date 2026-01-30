879db3f3beb1828230ac6980facb577733a6f589
Felix Jung <felix.jung@sumup.com>
2026-01-30 11:07:35 +0100

    fix: conditionally load config in main

--- cmd/trees/main.go
+++ cmd/trees/main.go
@@ -16,22 +16,11 @@ type appAPI interface {
 }
 
 func main() {
-	homeDir, err := os.UserHomeDir()
+	cfg, err := loadConfigIfNeeded(os.Args[1:])
 	if err != nil {
 		_, _ = os.Stderr.WriteString(err.Error() + "\n")
 		os.Exit(1)
 	}
-	configPath, err := configcmd.ResolvePath(os.Getenv, homeDir, os.Stat)
-	if err != nil {
-		_, _ = os.Stderr.WriteString(err.Error() + "\n")
-		os.Exit(1)
-	}
-	cfg, err := config.Load(configPath)
-	if err != nil {
-		_, _ = os.Stderr.WriteString(err.Error() + "\n")
-		os.Exit(1)
-	}
-
 	application, err := app.New(app.Deps{Runner: runner.OSRunner{Stdout: os.Stdout, Stderr: os.Stderr}, Stdout: os.Stdout}, cfg)
 	if err != nil {
 		_, _ = os.Stderr.WriteString(err.Error() + "\n")
@@ -50,4 +39,33 @@ func main() {
 		_, _ = os.Stderr.WriteString(err.Error() + "\n")
 		os.Exit(1)
 	}
+}
+
+func loadConfigIfNeeded(args []string) (*config.Config, error) {
+	command := firstNonFlagArg(args)
+	if command == "config" || command == "" {
+		return &config.Config{}, nil
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	configPath, err := configcmd.ResolvePath(os.Getenv, homeDir, os.Stat)
+	if err != nil {
+		return nil, err
+	}
+	return config.Load(configPath)
+}
+
+func firstNonFlagArg(args []string) string {
+	for _, arg := range args {
+		if arg == "--" {
+			return ""
+		}
+		if arg == "" || arg[0] == '-' {
+			continue
+		}
+		return arg
+	}
+	return ""
 }
